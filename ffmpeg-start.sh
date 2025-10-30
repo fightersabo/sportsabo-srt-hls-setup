3462299e902445330b217a1fedc794c8111f992f
fightersabo <116838610+fightersabo@users.noreply.github.com>
2025-10-30 05:51:29 +0300

    Update ffmpeg-start.sh

--- ffmpeg-start.sh
+++ ffmpeg-start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Load environment variables
+source /etc/srt-to-hls.env
+
 SRT_IN="srt://:${SRT_PORT}?mode=listener&latency=${SRT_LATENCY}&pkt_size=${PKT_SIZE}"
 
 if [ "$ENCODER" = "libx264" ]; then
@@ -8,19 +11,19 @@ if [ "$ENCODER" = "libx264" ]; then
       -c:v libx264 -preset veryfast -crf 23 \
       -c:a aac -b:a 128k \
       -f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments+program_date_time+append_list \
-      -hls_segment_type mpegts -hls_playlist_type event "${HLS_ROOT}/${HLS_PLAYLIST]}"
+      -hls_segment_type mpegts -hls_playlist_type event "${HLS_ROOT}/${HLS_PLAYLIST}"
 elif [ "$ENCODER" = "hevc_amf" ]; then
     ffmpeg -hide_banner -y -i "$SRT_IN" \
       -c:v hevc_amf -b:v 8M \
       -c:a aac -b:a 128k \
       -f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments+program_date_time+append_list \
-      -hls_segment_type mpegts -hls_playlist_type event "${HLS_ROOT}/${HLS_PLAYLIST]}"
+      -hls_segment_type mpegts -hls_playlist_type event "${HLS_ROOT}/${HLS_PLAYLIST}"
 elif [ "$ENCODER" = "h264_nvenc" ]; then
     ffmpeg -hide_banner -y -i "$SRT_IN" \
       -c:v h264_nvenc -b:v 8M \
       -c:a aac -b:a 128k \
       -f hls -hls_time 2 -hls_list_size 6 -hls_flags delete_segments+program_date_time+append_list \
-      -hls_segment_type mpegts -hls_playlist_type event "${HLS_ROOT}/${HLS_PLAYLIST]}"
+      -hls_segment_type mpegts -hls_playlist_type event "${HLS_ROOT}/${HLS_PLAYLIST}"
 else
     echo "Unknown encoder $ENCODER. Exiting."
     exit 1
